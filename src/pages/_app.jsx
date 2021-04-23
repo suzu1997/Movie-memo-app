29ffda7584254ba1e81290bd810408c2f4fdca49
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-04-23 13:53:08 +0900

    ♻️ _app.jsxにHeadタグを記載

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css';
+import Head from 'next/head';
+
+import 'src/styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>movilove!!</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://use.fontawesome.com/releases/v5.6.1/css/all.css'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;

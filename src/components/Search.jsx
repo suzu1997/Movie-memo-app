df8db22b5d0646c8c912e49d19d1bafd391bcf45
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-04-23 13:58:44 +0900

    ✨ 映画情報を取得する処理を追加

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,40 @@
-import classes from '../styles/components/Search.module.css';
-import { PrimaryButton } from './PrimaryButton';
+import { useState } from 'react';
+
+import { getMoviesData } from 'src/api/movies';
+import classes from 'src/styles/components/Search.module.css';
+import { PrimaryButton } from 'src/components/atoms/button/PrimaryButton';
+import { SearchButton } from 'src/components/atoms/button/SearchButton';
 
 export const Search = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=ad57d2dd7d865020bf69b242c5fa7428&language=ja&query=${searchText}`;
+
+  getMoviesData(apiUrl);
+
+  const seachTextChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleClickOpen = () => {
+    
+    alert('検索！！');
+  }
+
   return (
     <div className={classes.searchArea}>
       <input
         className={classes.input}
         type='text'
+        value={searchText}
+        onChange={seachTextChange}
         placeholder='タイトルから映画を検索'
       />
-      <div className={classes.searchIcon}>
+      <SearchButton onClick={handleClickOpen}>
         <i className='fas fa-search'></i>
-      </div>
-      <PrimaryButton>ジャンル検索</PrimaryButton>
+      </SearchButton>
+      <PrimaryButton type='submit'>ジャンル検索</PrimaryButton>
+    
     </div>
   );
 };

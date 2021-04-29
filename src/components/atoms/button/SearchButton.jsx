adf4df78b6f9fa8510483c9a3c909a772e13b068
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-04-29 16:31:06 +0900

    🩹 関数名の変更

--- src/components/atoms/button/SearchButton.jsx
+++ src/components/atoms/button/SearchButton.jsx
@@ -1,13 +1,13 @@
 import { memo } from "react";
 
 export const SearchButton = memo((props) => {
-  const { children, onClick, isSearchable } = props;
+  const { children, onClickSearch, isSearchable } = props;
   return (
     <div>
       {isSearchable ? (
-        <button className='rounded-full bg-gray-400 bg-opacity-50 w-11 h-11 text-center mr-3 focus:outline-none hover:bg-opacity-80' onClick={onClick} >{children}</button>
+        <button className='rounded-full bg-gray-400 bg-opacity-50 w-11 h-11 text-center mr-3 focus:outline-none hover:bg-opacity-80' onClick={onClickSearch} >{children}</button>
       ) : (
-        <button className='rounded-full bg-gray-400 bg-opacity-50 w-11 h-11 text-center mr-3 cursor-not-allowed' onClick={onClick} disabled>{children}</button>
+        <button className='rounded-full bg-gray-400 bg-opacity-50 w-11 h-11 text-center mr-3 cursor-not-allowed' onClick={onClickSearch}  disabled>{children}</button>
       )}
     </div>
   );

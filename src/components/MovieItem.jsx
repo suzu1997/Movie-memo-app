92acb8ea4dc4e900f1593dc4cac1d5ee11263b13
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-02 13:46:57 +0900

    💄 MovieItemのレスポンシブ適用とアイコンの変更

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -3,12 +3,12 @@ import { memo } from 'react';
 import { useSelectMovie } from 'src/hooks/useSelectMovie';
 
 export const MovieItem = memo((props) => {
-  const { selectedMovie, setSelectedMovie } = useSelectMovie();
+  const { setSelectedMovie } = useSelectMovie();
 
   const { movie } = props;
   const selectMovie = () => {
     setSelectedMovie(movie);
-  }
+  };
 
   const releaseYear = movie.release_date.slice(0, 4);
 
@@ -24,10 +24,23 @@ export const MovieItem = memo((props) => {
             alt='サムネイル'
             style={{ width: '50px', height: '50px' }}
           />
-          <p className='ml-4'>{`${movie.title}(${releaseYear}年)`}</p>
+          <p className='ml-4 text-sm sm:text-base'>{`${movie.title}(${releaseYear}年)`}</p>
         </div>
         <div className='flex items-center'>
-          <i className='fas fa-caret-right fa-2x'></i>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-4 w-4 sm:h-6 sm:w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M9 5l7 7-7 7'
+            />
+          </svg>
         </div>
       </div>
     </Link>

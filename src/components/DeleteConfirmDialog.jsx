1fa01107cf504c576e423541e06ec2e2224664c8
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-02 13:43:37 +0900

    ✨ 映画メモ削除の際の確認dialogを作成

---
+++ src/components/DeleteConfirmDialog.jsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export const DeleteConfirmDialog = memo((props) => {
+  const { open, handleClose, onClickDelete } = props;
+
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle id='form-dialog-title'>確認</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ⚠️本当にこの映画メモを削除しますか？
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary' className='focus:outline-none'>
+            キャンセル
+          </Button>
+          <Button onClick={onClickDelete} color='primary' className='focus:outline-none'>
+            削除する
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+});

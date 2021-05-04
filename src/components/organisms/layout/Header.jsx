1df09a43344660c634992ba2f9539a17fc29ee56
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-04 12:43:33 +0900

    ⚰️ 余分なclassNameの削除

--- src/components/organisms/layout/Header.jsx
+++ src/components/organisms/layout/Header.jsx
@@ -5,7 +5,7 @@ export const Header = memo(() => {
   return (
     <header className='text-blue-700 font-bold bg-blue-500 bg-opacity-30 w-full h-14 flex items-center'>
       <Link href='/'>
-        <div className='flex items-center cursor-pointer block h-14 pl-4'>
+        <div className='flex items-center cursor-pointer h-14 pl-4'>
           <i className='fas fa-film'></i> <p className='ml-1'>ムビラブ！！</p>
         </div>
       </Link>

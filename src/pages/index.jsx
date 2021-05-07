26ba4e41f01f6d82570a62ed2922fb6d321e060a
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-08 03:22:16 +0900

    🩹 コンポーネント名の変更による修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,14 @@
 import { useCallback, useState } from 'react';
 
-import { Footer } from 'src/components/organisms/layout/Footer';
-import { Header } from 'src/components/organisms/layout/Header';
-import { MyMovieList } from 'src/components/MyMovieList';
-import { Search } from 'src/components/Search';
-import { SearchResult } from 'src/components/SearchResult';
+import { Footer } from 'src/components/layout/Footer';
+import { Header } from 'src/components/layout/Header';
+import { MovieNotesList } from 'src/components/movie-note/MovieNotesList';
+import { SearchArea } from 'src/components/movie-search/SearchArea';
+import { SearchResult } from 'src/components/movie-search/SearchResult';
 
 export default function Home() {
   //Search, SearchResult のstateはindex.jsxで管理
-  const [open, setOpen] = useState(false);  //SearchResultを開くState
+  const [open, setOpen] = useState(false); //SearchResultを開くState
   const [searchText, setSearchText] = useState('');
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -16,20 +16,25 @@ export default function Home() {
   //映画の検索結果を開く関数
   const handleClickOpen = useCallback(() => {
     setOpen(true);
-  }, [setOpen]);
+  }, []);
 
   return (
     <div className='min-h-screen p-0 flex flex-col items-center'>
       <Header />
-      <Search
+      <SearchArea
         handleClickOpen={handleClickOpen}
         searchText={searchText}
         setSearchText={setSearchText}
         setSearchResult={setSearchResult}
         setLoading={setLoading}
       />
-      <MyMovieList />
-      <SearchResult open={open} setOpen={setOpen} searchResult={searchResult} loading={loading}/>
+      <MovieNotesList />
+      <SearchResult
+        open={open}
+        setOpen={setOpen}
+        searchResult={searchResult}
+        loading={loading}
+      />
       <Footer />
     </div>
   );

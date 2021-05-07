9f99d42f42df62df43e789c83146d1d3b5a24048
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-08 03:18:14 +0900

    🩹 selectedMovieの初期値を空文字に変更

--- src/providers/SelectedMovieProvider.jsx
+++ src/providers/SelectedMovieProvider.jsx
@@ -5,10 +5,10 @@ export const MovieContext = createContext();
 export const MovieContextProvider = (props) => {
   const { children } = props;
   const [selectedMovie, setSelectedMovie] = useState({
-    poster_path: '/n4nXOWVOn3Y2zYqtsMSQusVVxBt.jpg',
-    title: 'プラダを着た悪魔',
-    release_date: '2006-06-30',
-    overview: "恋に仕事にがんばるあなたへ贈る、ゴージャス＆ユーモラスなサクセスストーリー。大学を卒業し、ジャーナリストをめざしてNYにやってきたアンディ。オシャレに興味のない彼女が、一流ファッション誌‘RUNWAY’のカリスマ編集長ミランダ・プリーストリーのアシスタントに。そこは恐怖のポストだった！キャリアのためとはいえ、私生活はめちゃめちゃ。私って、本当は何をしたいんだっけ？"
+    poster_path: '',
+    title: '',
+    release_date: '',
+    overview: '',
   });
   return (
     <MovieContext.Provider value={{ selectedMovie, setSelectedMovie }}>

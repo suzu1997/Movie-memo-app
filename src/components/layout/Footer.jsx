32ae9720447a9e0befa03ad7ba40a025bae12d38
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-08 04:32:16 +0900

    📱  ヘッダーとフッターの高さをレスポンシブ対応

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 
 export const Footer = memo(() => {
   return (
-    <footer className='text-blue-700 bg-blue-500 bg-opacity-30 w-full h-16 flex justify-center items-center'>
+    <footer className='text-blue-700 bg-blue-500 bg-opacity-30 w-full h-12 sm:h-16 flex justify-center items-center'>
       &copy;Chie Suzuhara 2021
     </footer>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 
 export const Header = memo(() => {
   return (
-    <header className='text-blue-700 font-bold bg-blue-500 bg-opacity-30 w-full h-14 flex items-center'>
+    <header className='text-blue-700 font-bold bg-blue-500 bg-opacity-30 w-full h-12 sm:h-14 flex items-center'>
       <Link href='/'>
         <div className='flex items-center cursor-pointer h-14 pl-4'>
           <i className='fas fa-film'></i> <p className='ml-1'>ムビラブ！！</p>

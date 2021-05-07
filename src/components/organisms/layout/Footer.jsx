1ab4b9952e06ad08789d087307aaaaa099e9cfc8
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-07 20:59:50 +0900

    🔥 ディレクトリ構成の変更によるファイルの削除

--- src/components/organisms/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from 'react';
-
-export const Footer = memo(() => {
-  return (
-    <footer className='text-blue-700 bg-blue-500 bg-opacity-30 w-full h-16 flex justify-center items-center'>
-      &copy;Chie Suzuhara 2021
-    </footer>
-  );
-});

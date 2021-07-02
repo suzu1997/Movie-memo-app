8e21c57dad6d4a8c3ffd353f6e0c1a8524ce961b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-02 23:35:21 +0900

    ✨ サインアップ/サインインのフォームを実装して、トップページで表示切り替えできるようにした

---
+++ src/components/auth/SignIn.tsx
@@ -0,0 +1,77 @@
+import { VFC } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { signinWithEmailAndPassword } from 'src/firebase/auth';
+
+type Props = {
+  setIsLogin: (boolean: boolean) => void;
+}
+
+type Inputs = {
+  email: string;
+  passward: string;
+};
+
+export const SignIn: VFC<Props> = (props) => {
+  const { setIsLogin } = props;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = ({ email, passward }) => {
+    signinWithEmailAndPassword(email, passward);
+    console.log(email, passward);
+  };
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='font-bold text-lg mb-10'>ログイン</h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='w-11/12 flex flex-col'
+        >
+          {/* register your input into the hook by invoking the "register" function */}
+          <label>メールアドレス</label>
+          <input
+            className='w-full p-2 border-2 rounded-md border-gray-400 border-solid focus:outline-none focus:border-black'
+            placeholder='emailアドレスを入力してください'
+            type='text'
+            name='email'
+            {...register('email', { required: true })}
+          />
+          {errors.email ? (
+            <span className='text-red-400'>※必須項目です</span>
+          ) : null}
+          {/* include validation with required or other standard HTML validation rules */}
+          <label className='mt-8'>
+            パスワード<span className='text-sm'> ※6文字以上</span>
+          </label>
+          <input
+            className='w-full p-2 border-2 rounded-md border-gray-400 border-solid focus:outline-none focus:border-black'
+            placeholder='パスワードを入力してください'
+            type='password'
+            {...register('passward', { required: true, minLength: 6 })}
+          />
+          {/* errors will return when field validation fails  */}
+          {errors.passward?.type === 'required' ? (
+            <span className='text-red-400'>※必須項目です</span>
+          ) : null}
+          {errors.passward?.type === 'minLength' ? (
+            <span className='text-red-400'>※6文字以上で設定してください</span>
+          ) : null}
+          <br />
+          <input
+            type='submit'
+            value='ログイン'
+            className='text-white bg-green-700 text-xs sm:text-sm px-4 py-3 focus:outline-none rounded-lg hover:bg-opacity-90 w-20 m-auto mt-8'
+          />
+        </form>
+        <button className='text-blue-600 underline mt-4' onClick={() => setIsLogin(false)}>
+          新規登録はこちら
+        </button>
+      </div>
+    </>
+  );
+};
--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,15 +1,19 @@
 import { VFC } from 'react';
+import { useState } from 'react';
 
 import { Header } from 'src/components/layout/Header';
 import { Footer } from 'src/components/layout/Footer';
-import { SignUp } from 'src/components/SignUp';
+import { SignUp } from 'src/components/auth/SignUp';
+import { SignIn } from 'src/components/auth/SignIn';
 
 const Top: VFC = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className='min-h-screen p-0 flex flex-col items-center'>
       <Header />
-      <main className='min-h-full flex-grow flex flex-col justify-center bg-green-50 w-full'>
-        <SignUp />
+      <main className='min-h-full flex-grow flex flex-col justify-center bg-green-600 bg-opacity-10 w-full'>
+        {isLogin ? <SignIn setIsLogin={setIsLogin}/> : <SignUp setIsLogin={setIsLogin}/>}
       </main>
       <Footer />
     </div>
@@ -17,4 +21,3 @@ const Top: VFC = () => {
 };
 
 export default Top;
-

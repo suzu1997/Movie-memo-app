57546f7fc99dc1bbd7440bdc8cc0aff7fb5ee57b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-11 19:54:27 +0900

    🥅 エラーハンドリングを修正

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -6,11 +6,14 @@ export const signUpWithEmailAndPassword = async (
 ): Promise<any> => {
   try {
     const user = await auth.createUserWithEmailAndPassword(email, password);
+    if (!user) {
+      throw new Error('新規登録に失敗しました');
+    }
     alert('新規登録に成功しました');
     
     return user;
   } catch (error) {
-    console.error(error);
+    alert(error.message);
   }
 };
 
@@ -20,19 +23,21 @@ export const signinWithEmailAndPassword = async (
 ): Promise<any> => {
   try {
     const user = await auth.signInWithEmailAndPassword(email, password);
-    alert('サインイン成功');
+    if (!user) {
+      throw new Error('ログインに失敗しました');
+    }
+    alert('ログイン成功');
     
     return user;
   } catch (error) {
-    console.log(error);
+    alert(error.message);
   }
 };
 
 export const signOut = async (): Promise<void> => {
   try {
     await auth.signOut();
-
-    alert('サインアウト成功');
+    alert('サインアウト成功'); 
   } catch (error) {
     alert('サインアウト失敗');
     console.log(error);
